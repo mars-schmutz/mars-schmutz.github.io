075dd6db13caf3eae2be2732588305bf88043734
Marshall Schmutz <mschmutz@bagel.local>
2019-11-30 19:42:52 -0700

    work on portfolio page

---
+++ js/imgCompare.js
@@ -0,0 +1,6 @@
+// $(window).on('load', function() {
+//    $("#cd-container").twentytwenty();
+//  });
+new BeerSlider( document.getElementById( "beer-slider" ), { start: 25 } );
+
+new BeerSlider( document.getElementById( "beer-slider2" ), { start: 25 } );
--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,23 @@ function isInView(e) {
     return ((eBottom <= docBottom) && (eTop >= docTop));
 }
 
+// Lightboxes for portfolio page
+function displayImg(e) {
+    // console.log(e.dataset.image);
+    var lightBox = $("#" + e.dataset.image);
+    lightBox.css('display', 'block');
+}
+
+function displayCmp(e) {
+    var lightBox = $("#" + e.dataset.image);
+    lightBox.css('display', 'inline-block');
+}
+
+$('.lightbox-close').on('click', function() {
+    $('.lightbox').css('display', 'none');
+    $('#room-combo').css('display', 'none');
+});
+
 $(document).ready(function() {
     $(window).scroll(function() {
         $('.scroll-animated .animated').each(function() {
